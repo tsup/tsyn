f7fe01f9851eab85b3ca261e4d0755e4920d8f45
Marton Suranyi <marton.suranyi@gmail.com>
2013-04-28 12:30:12 +0200

    RingBuffer forward declared

--- src/Types.hpp
+++ src/Types.hpp
@@ -9,7 +9,10 @@ namespace tsyn
   class Sender;
   typedef std::unique_ptr< Sender > SenderRef;
 
+  template<typename T, size_t Size>
+  class RingBuffer;
+
   constexpr size_t RECEIVE_QUEUE_SIZE = 1024;
-  typedef RingBuffer< ReceiveQueueData, RECEIVE_QUEUE_SIZE > ReceiveQueue;
+  typedef RingBuffer< QueueData, RECEIVE_QUEUE_SIZE > ReceiveQueue;
 }
 
