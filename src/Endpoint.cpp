be629af7c8a2f4e086bfc11119ec29e118afed96
Marton Suranyi <marton.suranyi@gmail.com>
2013-05-15 16:50:19 +0200

    Endpoint error handling

--- src/Endpoint.cpp
+++ src/Endpoint.cpp
@@ -1,5 +1,6 @@
 #include "Endpoint.hpp"
 
+#include <algorithm>
 #include <system_error>
 
 tsyn::Endpoint::Endpoint( const std::string & endpointStr )
@@ -27,9 +28,45 @@ tsyn::Endpoint::Port tsyn::Endpoint::port() const
 
 namespace
 {
+void checkStringIndex( size_t index )
+{
+  if ( std::string::npos == index )
+    throw std::system_error();
+}
+
+void forEachToken( const std::string & in,
+                   char delimiter,
+                   std::function<void(const std::string&)> func )
+{
+  size_t next = 0;
+  size_t prev = 0;
+
+  while( next != std::string::npos )
+  {
+    next = in.find(delimiter,next+1);
+    func(in.substr(prev,next-prev));
+    prev = next+1;
+  }
+}
+
+void checkValidIp( const std::string & ip )
+{
+    if ( std::count(ip.begin(), ip.end(), '.') != 3 )
+      throw std::system_error();
+
+    char delimiter = '.';
+    forEachToken( ip, delimiter, []( const std::string & token )
+    {
+      int element = std::stoi(token);
+      if ( element < 0 || element > 255 )
+        throw std::system_error();
+    });
+}
+
 tsyn::Proto parseProto( const std::string & str )
 {
   size_t semiColonIndex = str.find( ':' );
+  checkStringIndex(semiColonIndex);
   if ( 0 == str.compare(0,semiColonIndex, "tcp" ) )
   {
     return tsyn::Proto::TCP;
@@ -43,15 +80,30 @@ tsyn::Proto parseProto( const std::string & str )
 
 tsyn::Endpoint::Port parsePort( const std::string & str )
 {
-  size_t lastSemiColon = str.find_last_of( ':' );
-  return std::stoi( str.substr( lastSemiColon+1 ) );
+  try
+  {
+    size_t lastSemiColon = str.find_last_of( ':' );
+    checkStringIndex(lastSemiColon);
+    int port = std::stoi( str.substr( lastSemiColon+1 ) );
+    if ( port <= 0 || port >= 65536 )
+      throw std::system_error();
+    return port;
+  }
+  catch(const std::invalid_argument & ex) { throw std::system_error(); }
+  catch(const std::out_of_range & ex) { throw std::system_error(); }
 }
 
 std::string parseIpAddress( const std::string & str )
 {
   size_t doubleSlashIndex = ( str.find( "//" )+2 ) ;
+  checkStringIndex(doubleSlashIndex);
+
   size_t secondSemiColonIndex = str.find( ':', doubleSlashIndex );
-  return str.substr( doubleSlashIndex, secondSemiColonIndex-doubleSlashIndex );
+  checkStringIndex(secondSemiColonIndex);
+
+  std::string ret = str.substr( doubleSlashIndex, secondSemiColonIndex-doubleSlashIndex );
+  checkValidIp(ret);
+  return ret;
 }
 
 }
--- tst/test_endpoint.cpp
+++ tst/test_endpoint.cpp
@@ -1,3 +1,4 @@
+#include <igloo/igloo.h>
 #include <igloo/igloo_alt.h>
 using namespace igloo;
 
@@ -28,6 +29,103 @@ Describe( AnEndpoint )
   {
     AssertThrows( std::system_error,
       tsyn::Endpoint endpoint( "xuuu://1.1.1.1:8080" )
+    );
+  }
+
+  Context(malformed_IP_address)
+  {
+    Spec(IP_has_element_greater_than_255)
+    {
+      AssertThrows( std::system_error,
+        tsyn::Endpoint endpoint( "tcp://89.1.455.2:9898" )
+      );
+    }
+
+    Spec(IP_has_not_enough_element)
+    {
+      AssertThrows( std::system_error,
+        tsyn::Endpoint endpoint( "tcp://1.219.2:9898" )
+      );
+    }
+
+    Spec(IP_has_too_many_elements)
+    {
+      AssertThrows( std::system_error,
+        tsyn::Endpoint endpoint( "tcp://1.2.4.219.2:9898" )
+      );
+    }
+  };
+
+  Context(invalid_port)
+  {
+    Spec(port_is_too_high)
+    {
+      AssertThrows( std::system_error,
+        tsyn::Endpoint endpoint( "tcp://43.88.9.45:70000" )
+      );
+    }
+
+    Spec(port_is_zero)
+    {
+      AssertThrows( std::system_error,
+        tsyn::Endpoint endpoint( "tcp://43.88.9.45:0" )
+      );
+    }
+
+    Spec(port_is_negative)
+    {
+      AssertThrows( std::system_error,
+        tsyn::Endpoint endpoint( "tcp://43.88.9.45:-12" )
+      );
+    }
+
+    Spec(port_is_text)
+    {
+      AssertThrows( std::system_error,
+        tsyn::Endpoint endpoint( "tcp://43.88.9.45:http" )
+      );
+    }
+  };
+
+  Context(input_string_is_malformed)
+  {
+    Spec(random_bullshit)
+    {
+      AssertThrows( std::system_error,
+        tsyn::Endpoint endpoint( "fortytwo dog ate my cap" )
+      );
+    }
+
+    Spec(only_proto)
+    {
+      AssertThrows( std::system_error,
+        tsyn::Endpoint endpoint( "tcp" )
+      );
+
+      AssertThrows( std::system_error,
+        tsyn::Endpoint endpoint( "tcp:" )
+      );
+    }
+
+    Spec(missing_IP)
+    {
+      AssertThrows( std::system_error,
+        tsyn::Endpoint endpoint( "tcp://:1024" )
+      );
+    }
+
+    Spec(only_separators)
+    {
+      AssertThrows( std::system_error,
+        tsyn::Endpoint endpoint( "://...:" )
+      );
+    }
+  };
+
+  It(cannot_resolve_hostname)
+  {
+    AssertThrows( std::system_error,
+      tsyn::Endpoint endpoint( "tcp://github.com:9898" )
     );
   }
 };
